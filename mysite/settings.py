20cf3e72dd78035809049063ac96357c0ee496eb
Daniil Tseselsky <dani@Daniils-MacBook-Air.local>
2017-09-25 15:32:01 +0300

    Change settings

--- mysite/settings.py
+++ mysite/settings.py
@@ -128,9 +128,9 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, 'static'),
+# )
 CKEDITOR_CONFIGS = {
     'awesome_ckeditor': {
         'toolbar': 'full',
